6d447efa9c2dc5a2d65b7dd1ab4ed547e4c7d9bb
Cris-Rub <cristopher.rubio6397@alumnos.udg.mx>
2023-10-20 18:32:56 -0600

    fix: cpu_usage

--- cpu_usage.py
+++ cpu_usage.py
@@ -1,6 +1,7 @@
 from gpiozero import LED
-import psutil from time import sleep
+import psutil
+from time import sleep
 from datetime import datetime
 led_yellow = LED(20)
 led_red = LED(21)
-file=open("log.txt","Y")
+file=open("log.txt","w")
