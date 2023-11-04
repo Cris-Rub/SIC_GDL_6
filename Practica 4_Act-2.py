d27a2b76c4ab8cb7461d55fb16a8a7da4fb8e67f
Armando Ruiz Anaya <99087774+ArmandoRuiz13@users.noreply.github.com>
2023-11-03 18:19:32 -0600

    Update Practica 4_Act-2.py
    alertando sobre el paquete

--- Practica 4_Act-2.py
+++ Practica 4_Act-2.py
@@ -1,35 +1,37 @@
 import RPi.GPIO as GPIO
 import time
 from hcsr04sensor import sensor
+#ocupa este paquete pip install hc-sr04
 
-# Configura los pines GPIO
+
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)  # Pin para el LED
 GPIO.setup(22, GPIO.IN)   # Pin para el botón
 
-# Abre el archivo de registro
+
 log_file = open('/home/pi/Unit_Practice/distance_log.txt', 'a')
 
 try:
-    # Crea un objeto del sensor ultrasónico
+   
     sr04 = sensor.Measurement(17, 18)
 
     while True:
-        # Espera hasta que se presione el botón
+        
         while not GPIO.input(22):
             time.sleep(0.1)
 
-        # Realiza la medición de distancia
+       
         distance = sr04.raw_distance()
 
-        # Define el intervalo deseado (ajusta este valor según tus necesidades)
+        
         intervalo_deseado = 10  # en centímetros
 
         if distance < intervalo_deseado:
             # Enciende el LED
             GPIO.output(17, True)
 
-        # Registra la distancia en el archivo de registro
+        
         log_file.write(f"Distance: {distance} cm\n")
         log_file.flush()
 
